8cc2416a44bd74d07a2036fd7f84a65e11ce21cb
谷水信吾 <stkboy3@gmail.com>
2021-05-30 14:09:54 +0900

    add responsive config

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,5 +1,14 @@
 document.addEventListener("turbolinks:load", function () {
 
+
+  // ハンバーガーメニュー
+  $(function () {
+    $('.menu-icon').on('click', function () {
+    $('.header-menus').toggleClass('open');
+    })
+  });
+
+
   // フラッシュ自動フェードアウト
   $(function(){
     setTimeout("$('.flash').fadeOut('slow')", 5000) 
